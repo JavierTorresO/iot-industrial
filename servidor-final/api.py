ffe20bdd2d1b7de4ce14912d7c9c8c508d205622
SonodaPopin <joahernandez2022@udec.cl>
2025-07-01 20:10:37 -0400

    Agregado de comentarios en el codigo

--- servidor-final/api.py
+++ servidor-final/api.py
@@ -9,18 +9,25 @@ app = Flask(__name__)
 
 #Funcion que recibe datos JSON desde el servidor intermedio
 def recibir_dato():
+
+    #Toma el mensaje y lo convierte en un diccionario Python
     data = request.get_json()
     if not data:
         return jsonify({'error': 'No se recibieron datos'}), 400
 
+    #Llama a "insertar_dato()" para guardar el dato en la BD
     try:
         insertar_dato(data)
         return jsonify({'mensaje': 'Dato almacenado correctamente'}), 201
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+#Define una ruta GET /api/datos usada por el Cliente consulta para obtener los ultimos registros
 @app.route('/api/datos', methods=['GET'])
 def get_datos():
+
+    #Llama a "obtener_ultimos_datos()" para conseguir los ultimos "limit" registros
+    #Y los devuelve en JSON
     try:
         datos = obtener_ultimos_datos()
         return jsonify(datos), 200
